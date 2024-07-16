f36b703e63c0506523d455468a2f8d86fc448efd
nakamura <hrm.nkmr04@gmail.com>
2024-07-17 07:31:47 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var age = 25;
+if (age < 18) {
+	console.log("falseの処理");
+	console.log("ageは未成年です。");
+} else if (age < 60) {
+	console.log("trueの処理");
+	console.log("ageは成人です。");
+} else {
+	console.log("trueの処理");
+	console.log("ageは高齢者です。");
+}
